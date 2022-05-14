2e06a8b59a8eb55d9342671c8e71abcfbbb07e77
AnnaPyrkh <69246658+AnnaPyrkh@users.noreply.github.com>
2022-05-14 10:36:53 +0300

    Add files via upload

---
+++ l1 - task 6.py
@@ -0,0 +1,11 @@
+# Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
+
+abc_number = int(input('Введите номер буквы в алфавите: '))
+
+# Генерация списка букв
+abc_list = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
+# print(abc_list)
+if abc_number <= len(abc_list):
+    print(f'Буква под номером {abc_number}: {abc_list[abc_number - 1]}')
+else:
+    print(f'Введённое число за пределами количества букв в алфавите ({len(abc_list)})')
